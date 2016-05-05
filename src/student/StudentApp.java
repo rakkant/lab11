0156436651870b0bf6b0f864d6d221024ff48c84
Rakkan Tiasakul <rakkantia@gmail.com>
2016-05-05 16:58:56 +0700

    replace with stream()

--- src/student/StudentApp.java
+++ src/student/StudentApp.java
@@ -20,21 +20,15 @@ public class StudentApp {
 	
 	
 	public void filterAndPrint( List<Student> students, Predicate<Student> filter, Consumer<Student>action, Comparator<Student> byName , Comparator<Student> byDate ) {
-		students.sort(byName);
-		//students.sort(byDate);
-		for(Student s : students ) {
-			if (filter.test(s))
-	                  action.accept(s);
-						
-						
-		}
+		students.stream().filter(filter).sorted(byDate).forEach(action);				
+					
 	}
 	
 	public static void main(String[] args) {
 		List<Student> students = Registrar.getInstance().getStudents();
 		LocalDate localDate = LocalDate.now();
 		Comparator <Student> byName = (a,b) -> a.getFirstname().charAt(0) - b.getFirstname().charAt(0);
-		Comparator <Student> byDate = (a,b) -> a.getBirthdate().getMonthValue() - b.getBirthdate().getMonthValue();
+		Comparator <Student> byDate = (a,b) -> a.getBirthdate().getDayOfMonth() - b.getBirthdate().getDayOfMonth();
 		Predicate<Student> filter = s -> s.getBirthdate().getMonthValue() == localDate.getMonthValue();
 		Consumer <Student> birthday = s -> System.out.printf("%s %s will have birthday on %d %s.\n", 
 				s.getFirstname(),s.getLastname(), s.getBirthdate().getDayOfMonth(),localDate.getMonth());
